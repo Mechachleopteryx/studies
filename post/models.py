fc3a9d3c33c4f60db3f1b30685a1012d4308f837
wozmin <tomaszwozniak.wzm@gmail.com>
2014-05-18 12:55:11 +0200

    Sample application with models

---
+++ post/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    tags = models.CharField(max_length=200, null=True, blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('post.Comment')
+    full_name = models.CharField(max_length=64)
+    value = models.TextField()
