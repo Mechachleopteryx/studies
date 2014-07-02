c8c6645953893e00575dd71e199e1b38649f252b
wozmin <tomaszwozniak.wzm@gmail.com>
2014-07-02 16:44:47 +0200

    presentation intro/models/orm/forms 100%

--- post/forms.py
+++ post/forms.py
@@ -12,3 +12,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ('post', )
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data['full_name']
+        if "donald" in full_name.lower():
+            raise forms.ValidationError("You cannot comment on this blog!")
+
+        # Always return the cleaned data, whether you have changed it or
+        # not.
+        return full_name
